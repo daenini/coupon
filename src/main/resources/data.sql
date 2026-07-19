5d9a913e9e5c5923cc0c654ba8c0d88c42931213
daeni <eogus8292@naver.com>
2026-07-20 01:34:03 +0900

    chore: 개발용 초기 데이터 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO coupon_policy (name, total_quantity, issued_quantity, start_at, end_at)
+VALUES ('선착순 치킨 쿠폰', 100, 0, '2026-07-01 10:00:00', '2026-07-31 10:00:00');
+
+INSERT INTO coupon_policy (name, total_quantity, issued_quantity, start_at, end_at)
+VALUES ('소진 테스트용 쿠폰', 1, 0, '2026-07-01 10:00:00', '2026-07-31 10:00:00');
